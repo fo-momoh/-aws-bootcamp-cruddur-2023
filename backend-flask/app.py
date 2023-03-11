4626ff677f8b385357716cb4577a8e2bcc03cff6
Foday Momoh <fo.momoh08@gmail.com>
2023-03-11 06:47:47 +0000

    configuration for successful authentication completed

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -15,7 +15,7 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
-from lib.cognito_jwt_token import CognitoJwtToken
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
 
 # HoneyComb
 from opentelemetry import trace
@@ -184,20 +184,19 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  
-  access_token = CognitoJwtToken.extract_access_token(request.headers)
+  access_token = extract_access_token(request.headers)
   try:
-      claims = cognito_jwt_token.token_service.verify(access_token)
-      self.claims = self.token_service.claims
-      g.cognito_claims = self.claims
+    claims = cognito_jwt_token.verify(access_token)
+    # authenticated request
+    app.logger.debug('authenticated')
+    app.logger.debug(claims)
+    app.logger.debug(claims['username'])
+    data = HomeActivities.run(cognito_user_id=claims['username'])
   except TokenVerifyError as e:
-      _ = request.data
-      abort(make_response(jsonify(message=str(e)), 401))
-
-  app.logger.debug('claims')
-  app.logger.debug(claims)
-
-  data = HomeActivities.run()
+    # unauthenticated request
+    app.logger.debug(e)
+    app.logger.debug('unauthenticated')
+    data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -3,7 +3,7 @@ from opentelemetry import trace
 
 tracer = trace.get_tracer("home-activities-mock-data")
 class HomeActivities:
-  def run():
+  def run(cognito_user_id=None):
     #logger.info("HomeActivities")
     with tracer.start_as_current_span("mock-data"):
       span = trace.get_current_span()
@@ -48,6 +48,19 @@ class HomeActivities:
         'replies': []
       }
       ]
+      
+      if cognito_user_id != None:
+        extra_crud = {
+          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+          'handle':  'Lore',
+          'message': "You're logged in!",
+          'created_at': (now - timedelta(hours=1)).isoformat(),
+          'expires_at': (now + timedelta(hours=12)).isoformat(),
+          'likes': 5995,
+          'replies': []
+        }
+        results.insert(0, extra_crud)
+
       span.set_attribute("app.result_length", len(results))
       return results
       
