2ac07a433622ea23841a381ee5572763a71816e8
Foday Momoh <fo.momoh08@gmail.com>
2023-03-16 21:37:42 +0000

    set up connection to prod db

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -17,5 +17,5 @@ def query_wrap_array(template):
   """
   return sql
 
-connection_url = os.getenv("CONNECTION_URL")
+connection_url = os.getenv("PROD_CONNECTION_URL")
 pool = ConnectionPool(connection_url)
