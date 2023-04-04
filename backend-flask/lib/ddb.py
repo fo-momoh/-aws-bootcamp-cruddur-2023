6823ff4d3bdb874a3e8f3872b8ec2a64739f1bd9
Foday Momoh <fo.momoh08@gmail.com>
2023-04-04 13:29:33 +0000

    updated db/drop scrip and initiated ddb.py

---
+++ backend-flask/lib/ddb.py
@@ -0,0 +1,15 @@
+import boto3
+import sys
+from datetime import datetime, timedelta, timezone
+import uuid
+import os
+
+class Ddb:
+  def client():
+    endpoint_url = os.getenv("AWS_ENDPOINT_URL")
+    if endpoint_url:
+      attrs = { 'endpoint_url': endpoint_url }
+    else:
+      attrs = {}
+    dynamodb = boto3.client('dynamodb',**attrs)
+    return dynamodb
