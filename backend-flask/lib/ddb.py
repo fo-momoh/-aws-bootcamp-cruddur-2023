f36b59ac04080095996ba19c4c157acc197f552e
Foday Momoh <fo.momoh08@gmail.com>
2023-04-07 15:55:12 +0000

    implemented list-messages

--- backend-flask/lib/ddb.py
+++ backend-flask/lib/ddb.py
@@ -14,15 +14,15 @@ class Ddb:
     dynamodb = boto3.client('dynamodb',**attrs)
     return dynamodb
   def list_message_groups(client,my_user_uuid):
-    # year = str(datetime.now().year)
+    year = str(datetime.now().year)
     table_name = 'cruddur-messages'
     query_params = {
       'TableName': table_name,
-      'KeyConditionExpression': 'pk = :pk',
+      'KeyConditionExpression': 'pk = :pk AND begins_with(sk,:year)',
       'ScanIndexForward': False,
       'Limit': 20,
       'ExpressionAttributeValues': {
-        # ':year': {'S': year },
+        ':year': {'S': year },
         ':pk': {'S': f"GRP#{my_user_uuid}"}
       }
     }
@@ -34,7 +34,6 @@ class Ddb:
     # query the table
     response = client.query(**query_params)
     items = response['Items']
-    
 
     results = []
     for item in items:
@@ -45,5 +44,35 @@ class Ddb:
         'handle': item['user_handle']['S'],
         'message': item['message']['S'],
         'created_at': last_sent_at
+      })
+    return results
+
+  def list_messages(client,message_group_uuid):
+    table_name = 'cruddur-messages'
+    year = str(datetime.now().year)
+    query_params = {
+      'TableName': table_name,
+      'KeyConditionExpression': 'pk = :pk AND begins_with(sk,:year)',
+      'ScanIndexForward': False,
+      'Limit': 20,
+      'ExpressionAttributeValues': {
+        ':year': {'S': year },
+        ':pk': {'S': f"MSG#{message_group_uuid}"}
+      }
+    }
+
+    response = client.query(**query_params)
+    items = response['Items'] 
+    #reverses the order in which messages appear
+    items.reverse()
+    results = []
+    for item in items:
+      created_at = item['sk']['S']
+      results.append({
+        'uuid': item['message_uuid']['S'],
+        'display_name': item['user_display_name']['S'],
+        'handle': item['user_handle']['S'],
+        'message': item['message']['S'],
+        'created_at': created_at
       })
     return results
