0c58bdd79a1f269767745a9ecab8714ab867ddac
Foday Momoh <fo.momoh08@gmail.com>
2023-04-15 17:40:07 +0000

    created and pushed base images for frontend-react-js

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -77,8 +77,8 @@ processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 
 # Will display logs in backend-flaskcontainer logs (STDOUT)
-simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
-provider.add_span_processor(simple_processor)
+#simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+#provider.add_span_processor(simple_processor)
 
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
