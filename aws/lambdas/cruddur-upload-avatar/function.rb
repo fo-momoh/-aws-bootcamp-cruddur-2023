4fa03ec32ea62ff04782122addf937e3a7dd3cec
Foday Momoh <fo.momoh08@gmail.com>
2023-05-08 00:40:27 +0000

    render avatar from CloudFront

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -1,53 +1,46 @@
-require 'aws-sdk-s3'
-require 'json'
-require 'jwt'
+# retrun CORS headers for preflight check
+if event['routeKey'] === "OPTIONS /{proxy+}"
+  puts({step: 'preflight', message: 'preflight CORS check'}.to_json)
+  { 
+    headers: {
+        "Access-Control-Allow-Headers": "*, Authorization",
+        "Access-Control-Allow-Origin": "https://3000-fomomoh-awsbootcampcrud-w3sh1yck1an.ws-us96b.gitpod.io",
+        "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
+    },
+    statusCode: 200 
+  }
+else
+  token = event['headers']['authorization'].split(' ')[1]
+  puts({step: 'presignedurl', access_token: token}.to_json)
 
-def handler(event:, context:)
-    puts event
-    
-    # retrun CORS headers for preflight check
-    if event['routeKey'] === "OPTIONS /{proxy+}"
-      puts({step: 'preflight', message: 'preflight CORS check'}.to_json)
-      { 
-        headers: {
-            "Access-Control-Allow-Headers": "*, Authorization",
-            "Access-Control-Allow-Origin": "https://3000-fomomoh-awsbootcampcrud-w3sh1yck1an.ws-us96b.gitpod.io",
-            "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
-        },
-        statusCode: 200 
-      }
-    else
-      token = event['headers']['authorization'].split(' ')[1]
-      puts({step: 'presignedurl', access_token: token}.to_json)
+  body_hash = JSON.parse(event["body"])
+  extension = body_hash["extension"]
 
-      body_hash = JSON.parse(event["body"])
-      extension = body_hash["extension"]
+  decoded_token = JWT.decode token, nil, false
+  puts "decodedtoken"
+  cognito_user_uuid = decoded_token[0]['sub']
 
-      decoded_token = JWT.decode token, nil, false
-      puts "decodedtoken"
-      cognito_user_uuid = decoded_token[0]['sub']
+  s3 = Aws::S3::Resource.new
+  bucket_name = ENV["UPLOADS_BUCKET_NAME"]
+  object_key = "#{cognito_user_uuid}.#{extension}"
 
-      s3 = Aws::S3::Resource.new
-      bucket_name = ENV["UPLOADS_BUCKET_NAME"]
-      object_key = "#{cognito_user_uuid}.jpg"
+  puts({object_key: object_key}.to_json)
+
+  obj = s3.bucket(bucket_name).object(object_key)
+  url = obj.presigned_url(:put, expires_in: 300)
+  url # this is the data that will be returned
+  body = {url: url}.to_json
+  { 
+    headers: {
+        "Access-Control-Allow-Headers": "*, Authorization",
+        "Access-Control-Allow-Origin": "https://3000-fomomoh-awsbootcampcrud-w3sh1yck1an.ws-us96b.gitpod.io",
+        "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
+    },
+    statusCode: 200, 
+    body: body     
   
-      puts({object_key: object_key}.to_json)
-
-      obj = s3.bucket(bucket_name).object(object_key)
-      url = obj.presigned_url(:put, expires_in: 300)
-      url # this is the data that will be returned
-      body = {url: url}.to_json
-      { 
-        headers: {
-            "Access-Control-Allow-Headers": "*, Authorization",
-            "Access-Control-Allow-Origin": "https://3000-fomomoh-awsbootcampcrud-w3sh1yck1an.ws-us96b.gitpod.io",
-            "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
-        },
-        statusCode: 200, 
-        body: body     
-      
-      }
-    end
+  }
+end
 end
 
 # puts handler(
--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -1,8 +1,11 @@
 import './ProfileHeading.css';
 import EditProfileButton from '../components/EditProfileButton';
 
+import ProfileAvatar from './ProfileAvatar';
+
+
 export default function ProfileHeading(props) {
-  const backgroundImage = 'url("https://assets.cruddur.promomoh.org/banners/galaxy.jpg")';
+  const backgroundImage = 'url("https://assets.cruddur.promomoh.org/banners/airj.jpg")';
   const styles = {
     backgroundImage: backgroundImage,
     backgroundSize: 'cover',
@@ -13,9 +16,7 @@ export default function ProfileHeading(props) {
     <div className='title'>{props.profile.display_name}</div>
     <div className="cruds_count">{props.profile.cruds_count} Cruds</div>
     <div className="banner" style={styles}>
-      <div className="avatar">
-          <img src="https://assets.cruddur.promomoh.org/avatars/data1.jpg"></img>
-      </div>
+      <ProfileAvatar id={props.profile.cognito_user_uuid}></ProfileAvatar>
     </div>
     <div className="info">
       <div className="id">
